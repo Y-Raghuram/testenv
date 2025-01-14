193f32359c8a63611b584d04b1ebcd638c876e6c
Y-Raghuram <y.raghuram01@gmail.com>
2025-01-14 02:11:16 -0700

    deploy

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,71 @@
 import React, { Component } from 'react';
+import html from '../logo/html.png';
+
+import css from '../logo/css.png'
+import js from '../logo/js.png'
+import man_workings from '../logo/man_workings.png'
+import sql from '../logo/sql.png'
+import dotnet from '../logo/dotnet.png'
+import c from '../logo/c#.jpg'
+import java from '../logo/java.png'
+import react from '../logo/react.png'
+import {AnimationOnScroll} from 'react-animation-on-scroll'
+import "animate.css/animate.compat.css"
+
+
+
+const element_four = {
+    
+   marginTop: '50px',
+   marginLeft: '400px'
+  
+   
+ }
+
+const element_five = {
+    
+   marginTop: '-450px',
+   marginLeft: '250px'
+ }
+
+ const element_six = {
+   
+   marginTop: '50px',
+   marginLeft: '170px'
+   
+ }
+
+ const element_seven = {
+   
+   marginTop: '50px',
+   marginLeft: '220px'
+ }
+
+ const element_eight = {
+   
+   marginTop: '-180px',
+   marginLeft: '1200px'
+ }
+
+ const element_nine = {
+   
+   marginTop: '-390px',
+   marginLeft: '650px'
+ }
+
+ const element_ten = {
+   
+   marginTop: '80px',
+   marginLeft: '700px'
+ }
+
+ const element_eleven = {
+   
+   marginTop: '90px',
+   marginLeft: '700px'
+
+ }
+
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -6,38 +73,65 @@ export default class About extends Component {
       <section id="about">
          <div className="row">
 
-            <div className="three columns">
+           
+            {/* <div className="nine columns main-col">
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+               
+            </div> */}
 
-            </div>
+      <AnimationOnScroll style={element_four} animateIn="fadeIn"  animateOut="fadeOut"  duration={2.0}>
+        
+        
+        <img style={{ height: 550, marginLeft: -200, marginTop: 0  }} src={man_workings} alt="Logo" />
+        
+        </AnimationOnScroll>
 
-            <div className="nine columns main-col">
+        <AnimationOnScroll style={element_five} animateIn="bounceInLeft"  animateOut="fadeOut"  duration={2.0}>
+        
+        
+        <img style={{ height: 80, marginTop: -100  }} src={html} alt="Logo" />
+        
+        </AnimationOnScroll>
 
-               <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
+        <AnimationOnScroll style={element_six} animateIn="bounceInLeft"  animateOut="fadeOut"  duration={2.0}>
+        
+        <div class="rolling">
+        <img style={{ height: 100  }} src={css} alt="Logo" />
+        <span >Let's Share daskljdlkakdad sajdkjsa;da;l </span>
+    
+        </div>
+        </AnimationOnScroll>
 
-               <div className="row">
+        <AnimationOnScroll style={element_seven} animateIn="bounceInLeft"  animateOut="fadeOut"  duration={2.0}>
+        
+        
+        <img style={{ height: 80  }} src={js} alt="Logo" />
+        
+        </AnimationOnScroll>
 
-                  <div className="columns contact-details">
+       
 
-                  <h2>Contact Details</h2>
-                  <p className="address">
-       						<span>{resumeData.name}</span>
-                     <br></br>
-       						   <span>
-                     {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.website}</span>
-       					   </p>
-                  </div>
-               </div>
-            </div>
+        <AnimationOnScroll style={element_nine} animateIn="bounceInRight"  animateOut="fadeOut"  duration={2.0}>
+        
+        
+        <img style={{ height: 80  }} src={sql} alt="Logo" />
+        
+        </AnimationOnScroll>
+
+        <AnimationOnScroll style={element_ten} animateIn="bounceInRight"  animateOut="fadeOut"  duration={2.0}>
+        
+        
+        <img style={{ height: 80  }} src={react} alt="Logo" />
+        
+        </AnimationOnScroll>
+
+        <AnimationOnScroll style={element_eleven} animateIn="bounceInRight"  animateOut="fadeOut"  duration={2.0}>
+        
+        
+        <img style={{ height: 60,marginLeft:-100   }} src={dotnet} alt="Logo" />
+        
+        </AnimationOnScroll>
+
          </div>
       </section>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -64,58 +64,7 @@ export default class Header extends Component {
       </header>
       
       </Parallax>
-      <Parallax bgImage= { image1 }  strength={-200}>
-      <header id="home">
-         <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-            <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
-            </ul>
-         </nav>
-
-         <div className="row banner">
-            <div className="banner-text">
-            <h1 className="responsive-headline">
-            <span style={{ fontSize: '50px' }}>I am</span> <br />
-            <FancyText gradient={{ from: '#F858E0', to: '#77156C', type: 'linear' }}
-            animateTo={{ from: '#6DEDD0', to: '#7AE23A' }}
-            animateDuration={2000}>
-              Raghuram 
-               </FancyText>
-               </h1>
-               <h2>
-               <TypeWriterEffect
-            textStyle={{ color: 'white', fontSize: 60, fontWeight: 'bold', fontFamily: 'Courier New'  }}
-            startDelay={100}
-            cursorColor="black"
-            text="< web  developer />"
-            typeSpeed={150}
-            
-          />
-          
-          </h2>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}> I am a Full Stack Developer based in US. worked on more <br/>  10 web  projects  proficient in frontend, backend and database 
-               ,currently <br/> working as full stack web developer at starmark software<br/>
-               </h3>
-              
-              
-            </div>
-         </div>
-
-         <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-         </p>
-
-      </header>
-      
-      
-    </Parallax>
+     
     </React.Fragment>
       
     );
